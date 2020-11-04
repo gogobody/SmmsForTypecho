9fe7f46aaec5f7304ffff64daa0137f386d63727
gogobody <138950128@qq.com>
2020-11-04 15:20:11 +0800

    fix bugs

--- js/comment.js
+++ js/comment.js
@@ -4,24 +4,27 @@ smms = {
             insertAtCaret: function (myValue) {
                 var $t = $(this)[0];
                 //IE
-                if (document.selection) { //||'input[data-addarea]'
+                if (document.selection) {
                     this.focus();
                     sel = document.selection.createRange();
                     sel.text = myValue;
                     this.focus();
+                    $(this).focus();
                 } else{
                     //!IE
-                    if ($t.selectionStart || $t.selectionStart == "0") {
+                    if ($t.selectionStart || $t.selectionStart === "0" ) {
                         var startPos = $t.selectionStart;
                         var endPos = $t.selectionEnd;
                         var scrollTop = $t.scrollTop;
-                        $t.value = $t.value.substring(0, startPos) + myValue + $t.value.substring(endPos, $t.value.length);
+                        // $t.value = $t.value.substring(0, startPos) + myValue + $t.value.substring(endPos, $t.value.length);
+                        var tval = $t.value.substring(0, startPos) + myValue + $t.value.substring(endPos, $t.value.length);
+                        $(this).val(tval)
                         this.focus();
                         $t.selectionStart = startPos + myValue.length;
                         $t.selectionEnd = startPos + myValue.length;
                         $t.scrollTop = scrollTop;
                     } else {
-                        this.value += myValue;
+                        $(this).val(this.val()+myValue)
                         this.focus();
                     }
                 }
@@ -53,8 +56,13 @@ smms = {
                         $("#zz-img-add .chevereto-pup-button-text").text('上传');
                         $('#zz-img-show').append('<img src="' + res.data.url + '" />');
                         //$('textarea[name="comment"]').val($('textarea[name="comment"]').val() + '<img src="' + res.data.url + '" />').focus();
-                        if (typeof comment_selector_!="undefined" ){
+                        if (typeof comment_selector_!="undefined" && $(comment_selector_).length > 0){
                             $(comment_selector_).insertAtCaret('<img src="' + res.data.url + '" />');
+                        }
+                        var onecircleIndexInput = $('input[data-addarea]')
+                        if (onecircleIndexInput.length > 0){
+                            onecircleIndexInput.insertAtCaret(res.data.url)
+                            onecircleIndexInput.siblings("button").click()
                         }
                     }
                 })
